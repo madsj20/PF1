c452e1d8ae43e3fd93b4a1ddb2fc1f1a6c90b469
Jakob Thiberg <jakob.t.thiberg@gmail.com>
2022-11-24 18:44:14 +0100

    Kicking ballz

--- Assets/Scripts/Kicking.cs
+++ Assets/Scripts/Kicking.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Kicking : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Drag in the Bullet Emitter from the Component Inspector.
+    public GameObject Kicker;
+
+    //Drag in the Bullet Prefab from the Component Inspector.
+    public GameObject Foot;
+
+    //Enter the Speed of the Bullet from the Component Inspector.
+    public float Foot_Forward_Force;
+
+    // Use this for initialization
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space"))
+        {
+            //The Bullet instantiation happens here.
+            GameObject Temporary_Bullet_Handler;
+            Temporary_Bullet_Handler = Instantiate(Foot, Kicker.transform.position, Kicker.transform.rotation) as GameObject;
+
+            //Sometimes bullets may appear rotated incorrectly due to the way its pivot was set from the original modeling package.
+            //This is EASILY corrected here, you might have to rotate it from a different axis and or angle based on your particular mesh.
+            Temporary_Bullet_Handler.transform.Rotate(Vector3.left * 90);
+
+            //Retrieve the Rigidbody component from the instantiated Bullet and control it.
+            Rigidbody Temporary_RigidBody;
+            Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody>();
+
+            //Tell the bullet to be "pushed" forward by an amount set by Bullet_Forward_Force. 
+            Temporary_RigidBody.AddForce(transform.forward * Foot_Forward_Force);
+
+            //Basic Clean Up, set the Bullets to self destruct after 10 Seconds, I am being VERY generous here, normally 3 seconds is plenty.
+            Destroy(Temporary_Bullet_Handler, 0.1f);
+        }
     }
 }
